7da93b903cfe84ae3742b11dd73904ef2fc8ec42
ad08fee3 <earnold1993@gmail.com>
2025-12-01 17:50:13 -0800

    2025 01 Part 2 Shorter

--- 2025/days/01/01.ts
+++ 2025/days/01/01.ts
@@ -2,26 +2,19 @@
 
 // Part 2
 export function adventMain(input: string): any {
-    let pointer = 50;
     const lines = input.split('\n');
+    let pointer = 50;
     let count = 0;
     lines.forEach((line: string) => {
-        const arr: string[] = line.split('');
-        const tickRight: boolean = (arr.shift() === "R");
-        if (pointer === 0 && !tickRight) {
-            // Resetting to 100 if we're going left prevents double counting.
-            pointer = 100;
-        }
-        const clicks: number = parseInt(arr.join(''));
-        pointer += clicks * ((tickRight) ? 1 : -1);
-        if (pointer >= 100 || pointer <= 0) {
-            count += Math.abs(Math.floor(pointer/100)); // Count how many times we roll over.
-            if (pointer <= 0 && pointer % 100 === 0) {
-                count++; // If we're at -100, that means we wrapped an extra time!
+        let clicks: number = parseInt(line.slice(1));
+        while(clicks-- > 0) {
+            if (pointer === 0) {
+                count++;
             }
+            pointer += (((line.charAt(0) === "R")) ? 1 : -1);
+            pointer %= 100;
+            pointer = (pointer < 0) ? pointer + 100 : pointer;
         }
-        pointer %= 100;
-        pointer = (pointer < 0) ? pointer + 100 : pointer;
     });
     return count;
 }
